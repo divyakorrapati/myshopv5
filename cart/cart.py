29d5d369e4acbdd535813428623f08e2a371188d
babonaresh <nbashaboena@unomaha.edu>
2018-12-01 16:29:36 -0600

    updated code with some UI changes

--- cart/cart.py
+++ cart/cart.py
@@ -43,7 +43,7 @@ class Cart(object):
         """
         return sum(item['quantity'] for item in self.cart.values())
 
-    def add(self, product, quantity=1, size=str, color=str, update_quantity=True,update_size=True,update_color=True):
+    def add(self, product, quantity=1, size=str, color=str, update_quantity=False,update_size=False,update_color=False):
         """
         Add a product to the cart or update its quantity.
         """
@@ -67,6 +67,7 @@ class Cart(object):
         else:
             self.cart[product_id]['color'] = color
         self.save()
+
 
     def save(self):
         # mark the session as "modified" to make sure it gets saved
